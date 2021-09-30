3306b03b1bb5283d31d3152761bfdf727de633cf
fkb56 <fkcb@live.fr>
2021-09-30 16:18:32 +0200

    Modification page d'acceuil

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="text-center text-6xl text-red-500">Hello world !!!</div>
+	<div class="text-center text-6xl mt-40">Bienvenue sur</div>
+	<div class="text-center text-4xl mt-10">
+		Laravel + Vue.js + Tailwindcss + Vite.js
+	</div>
 </template>
 
 <script></script>
