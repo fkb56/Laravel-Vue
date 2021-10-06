0d9cb2d905c4624fd0ef37d90726a071ff399761
fkb56 <fkcb@live.fr>
2021-10-06 17:24:38 +0200

    Ajout de la vue maitre

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<Nav></Nav>
+	<router-view></router-view>
+</template>
+
+<script>
+import Nav from "../Layouts/Nav.vue";
+
+export default {
+	components: { Nav },
+};
+</script>
+<style lang="scss" scoped></style>
